7e2ee3781ad03ca8226b55ff2b919d1199c025be
Juwon-14 <wndnjs9127@gmail.com>
2024-11-02 21:21:26 -0700

    Update main.cpp

--- Activity 27/main.cpp
+++ Activity 27/main.cpp
@@ -2,13 +2,24 @@
 //  main.cpp
 //  Activity 27
 //
-//  Created by 홍주원 on 11/2/24.
+//  Created by Juwon Hong on 11/2/24.
 //
 
 #include <iostream>
+#include "BookStore.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    BookStore myStore = {"Berkeley Bookstore", "456 Library Rd."};
+
+    myStore.addBook({"Data Science Essentials", "Tim", "9781234567890", 5, Date(2024, 11, 3)});
+    myStore.addBook({"Introduction to Algorithms", "Tom", "9780262033848", 3, Date(2024, 11, 4)});
+
+    Person customer("Juwon Hong", "Juwon@abcd.com");
+
+    std::string query = "Data Science Essentials";
+    Book* foundBook = myStore.searchBook(query);
+    std::cout << (foundBook ? "Success! Found the book: " + foundBook->title + " by " + foundBook->author + ".\n"
+                            : "Sorry, we couldn't find that book in our inventory.\n");
+
     return 0;
 }
